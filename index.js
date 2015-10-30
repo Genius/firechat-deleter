b2236a471debc9ad9538ac02335cd2ba1f732951
Mat Brown <mat@genius.com>
2015-10-30 09:13:45 -0400

    Use a queue to process rooms one at a time

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var async = require('async');
 var es = require('event-stream');
 var JSONStream = require('JSONStream');
 var lodash = require('lodash');
@@ -7,42 +8,68 @@ var request = require('request');
 var FirechatDeleter = function(firechatUrl, firechatSecret, rateLimitParams) {
   this.firechatUrl = firechatUrl;
   this.firechatSecret = firechatSecret;
+
+  if (rateLimitParams) {
+    this._deleteMessage = rateLimit(
+      rateLimitParams.count,
+      rateLimitParams.interval,
+      this._deleteMessage
+    );
+  }
 };
+
+var totalCount = 0;
+var deletedCount = 0;
 
 lodash.assign(FirechatDeleter.prototype, {
   deleteOlder: function(oldestTimestamp) {
+    var queue = async.queue(function(room, roomCallback) {
+      console.log('downloading messages for', room.name);
+      var messagesStream = this._getMessagesStream(room);
+      messagesStream.pipe(
+        es.map(function(message, messageCallback) {
+          if (message.timestamp < oldestTimestamp) {
+            this._deleteMessage(room, message, function() {
+              messageCallback();
+            });
+          } else {
+            messageCallback();
+          }
+        }.bind(this))
+      ).on('end', function() { roomCallback(); });
+    }.bind(this));
+
     this._getRoomsStream().pipe(
       es.mapSync(function(room) {
-        this._getMessagesStream(room).pipe(
-          es.mapSync(function(message, messageId) {
-            if (message.timestamp < oldestTimestamp) {
-              this._deleteMessage(room, message);
-            }
-          }.bind(this))
-        );
-      }.bind(this))
-    );
+        queue.push(room);
+      })
+    )
   },
 
   _getRoomsStream: function() {
-    return es.readArray([
-      {id: "-JZKhsBsZqjtlv64-yN5"},
-      {id: "-JZLODE1YXYl7XUbfHCS"}
-    ]);
-
-    return request({url: this._getAllRoomsUrl()}).
+    return request({url: this._getAllRoomsUrl() + '?auth=' + this.firechatSecret}).
       pipe(JSONStream.parse('*'));
   },
 
   _getMessagesStream: function(room) {
-    return request({url: this._getAllMessagesUrl(room.id)}).
+    return request({url: this._getAllMessagesUrl(room.id) + '?auth=' + this.firechatSecret}).
       pipe(JSONStream.parse('*', function(message, messageIdTuple) {
         return lodash.assign({id: messageIdTuple[0]}, message);
       }));
   },
 
-  _deleteMessage: function(room, message) {
-    console.log("DELETE", this._messageUrl(room.id, message.id), message.timestamp);
+  _deleteMessage: function(room, message, callback) {
+    var url = this._messageUrl(room.id, message.id);
+
+    request.del(url + '?auth=' + this.firechatSecret).
+      on('error', function(err) { console.error(err, url); }).
+      on('response', function(response, body) {
+        if (response.statusCode !== 200) console.error(response.statusCode, url, body);
+        if (!(++count % 100)) {
+          console.log('deleted', count, 'messages');
+          callback();
+        }
+      });
   },
 
   _getAllRoomsUrl: function() {
